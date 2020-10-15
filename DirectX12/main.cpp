9115dd3d99b189c017fd6559efeea13954bb42dc
Subroto <subrotoph@gmail.com>
2020-10-15 18:07:27 +0900

    Create input layout

--- DirectX12/main.cpp
+++ DirectX12/main.cpp
@@ -253,6 +253,15 @@ bool InitD3D() {
     pixelShaderBytecode.BytecodeLength = pixelShader->GetBufferSize();
     pixelShaderBytecode.pShaderBytecode = pixelShader->GetBufferPointer();
 
+    // create input layout
+    D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
+        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+    };
+
+    D3D12_INPUT_LAYOUT_DESC inputLayoutDesc = {};
+    inputLayoutDesc.NumElements = sizeof(inputLayout) / sizeof(D3D12_INPUT_ELEMENT_DESC);
+    inputLayoutDesc.pInputElementDescs = inputLayout;
+
     return true;
 }
 

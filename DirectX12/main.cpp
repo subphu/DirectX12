210d2a198f2735eb708230f49889bbdba0b7c350
Subroto <subrotoph@gmail.com>
2020-10-15 15:40:51 +0900

    Create device

--- DirectX12/main.cpp
+++ DirectX12/main.cpp
@@ -19,6 +19,41 @@ void mainloop() {
     }
 }
 
+bool InitD3D() {
+    HRESULT hr;
+
+    // Creating the Direct3D Device
+    IDXGIFactory4* dxgiFactory;
+    hr = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
+    if (FAILED(hr)) return false;
+
+    IDXGIAdapter1* adapter;
+    int adapterIndex = 0;
+    bool adapterFound = false;
+
+    while (dxgiFactory->EnumAdapters1(adapterIndex, &adapter) != DXGI_ERROR_NOT_FOUND) {
+        DXGI_ADAPTER_DESC1 desc;
+        adapter->GetDesc1(&desc);
+
+        if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) {
+            adapterIndex++;
+            continue;
+        }
+
+        hr = D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr);
+        if (SUCCEEDED(hr)){
+            adapterFound = true;
+            break;
+        }
+
+        adapterIndex++;
+    }
+
+    if (!adapterFound) return false;
+
+    hr = D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device));
+    if (FAILED(hr)) return false;
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nShowCmd) {
 
